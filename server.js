f04424b58101d6e2b6c4de1c6cef9b566724100c
Haider Zaidi <100717066+haiderzaidi07@users.noreply.github.com>
2024-07-09 18:02:10 +0500

    Created server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const PORT = 3100
+
+app.get('/', (req, res) => {
+        res.send({var1: "Test", var2: 2, var3: true})
+})
+
+app.listen(PORT, () => {
+        console.log(`Server running on port: ${PORT}`)
+})
