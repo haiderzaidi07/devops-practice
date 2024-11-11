4b023d8ac94c09f2f090677956ddf26245b76fd9
haiderzaidi07 <haiderzaidi.work@gmail.com>
2024-11-11 14:59:08 +0500

    solving issue 9

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = 3100
 
 app.get('/', (req, res) => {
-        res.send({var1: "Test", var2: 2, var3: true})
+        res.send({var1: "Test from issue 9", var2: 2, var3: true})
 })
 
 app.listen(PORT, () => {
